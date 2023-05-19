87283203a9afb2ecfbb488c605931bf4ae83fcae
Camellia Redko <camelliaredko@gmail.com>
2023-05-19 15:38:43 +0200

    hamburger menu nearly done

--- camellia_portfolio_project1_files/main.js
+++ camellia_portfolio_project1_files/main.js
@@ -4,16 +4,34 @@ document.addEventListener("DOMContentLoaded", function() {
   nav.classList.remove('menu-opened');
   let menu = 'closed';
 
-
+  // Event listener for burger menu click
   burger.addEventListener("click", function() {
-    if (menu == 'closed') {
+    if (menu === 'closed') {
       menu = 'opened';
       nav.classList.add('menu-opened');
+      document.querySelectorAll('.line-burger').forEach(function(line) {
+        line.classList.add('animate');
+      });
     } else {
       menu = 'closed';
       nav.classList.remove('menu-opened');
+      document.querySelectorAll('.line-burger').forEach(function(line) {
+        line.classList.remove('animate');
+      });
     }
-  }
-  , false);
+  });
 
+  // Event listener for clicks outside of the menu
+  document.addEventListener("click", function(event) {
+    const target = event.target;
+    if (!nav.contains(target) && !burger.contains(target)) {
+      if (menu === 'opened') {
+        menu = 'closed';
+        nav.classList.remove('menu-opened');
+        document.querySelectorAll('.line-burger').forEach(function(line) {
+          line.classList.remove('animate');
+        });
+      }
+    }
+  });
 });
