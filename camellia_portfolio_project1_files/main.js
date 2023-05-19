d3e7662f9a625ec56a90277a609f076ecffc900d
Camellia Redko <camelliaredko@gmail.com>
2023-05-19 13:07:08 +0200

    javascript file added

---
+++ camellia_portfolio_project1_files/main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const burger = document.getElementById('hamburger');
+  const nav = document.getElementById('nav');
+  nav.classList.remove('menu-opened');
+  let menu = 'closed';
+
+
+  burger.addEventListener("click", function() {
+    if (menu == 'closed') {
+      menu = 'opened';
+      nav.classList.add('menu-opened');
+    } else {
+      menu = 'closed';
+      nav.classList.remove('menu-opened');
+    }
+  }
+  , false);
+
+});
